222807c690e6f437c7ecf18ef428b1bd390d4e4a
SayedY00 <elsayedmoharam000@gmai.com>
2023-06-07 17:13:44 +0400

    Created the laning page and Bootstrap slider

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 let mySlider = document.querySelector(".slider"),
     upperBar = document.querySelector(".upper-bar"),
     navBar = document.querySelector(".navbar");
+    imgs = document.querySelectorAll(".carousel-item");
 
 window.onload = function () {
     mySlider.style.height = `${window.innerHeight - (upperBar.clientHeight + navBar.clientHeight)}px`;
+    imgs.forEach(img => {
+        img.style.height = `${window.innerHeight - (upperBar.clientHeight + navBar.clientHeight)}px`;
+    });
 }
     
